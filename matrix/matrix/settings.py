c7ba7bd7294ffac432123be9c8b7390870d9660e
Harish <harish0796@gmail.com>
2019-05-10 15:51:19 +0530

    updated database to postgres

--- matrix/matrix/settings.py
+++ matrix/matrix/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'matrix.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'matrix_database',
+        'USER': 'postgres',
+        'PASSWORD': 'blahblah',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
